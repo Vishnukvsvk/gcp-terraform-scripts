838e824227c629333bef29194847b9228ce979eb
Kvs Vishnu Kumar <36696407+Vishnukvsvk@users.noreply.github.com>
2021-04-07 11:58:16 +0530

    Update variables.tf

--- sql_database_instance/variables.tf
+++ sql_database_instance/variables.tf
@@ -1,4 +1,8 @@
 variable "gcp_service_list" {
   description = "List of GCP service to be enabled for a project."
   type        = list
+  default = [
+              "cloudresourcemanager.googleapis.com", 
+              "sqladmin.googleapis.com"
+            ]
 }
